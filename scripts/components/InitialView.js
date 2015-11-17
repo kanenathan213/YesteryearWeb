dd52d876f4df77fd09b08451a39e03ba488789a8
Nathan Kane <kanenathan213@gmail.com>
2015-11-17 19:44:40 +0700

    1-updated some styling, placeholders for initial view

--- scripts/components/InitialView.js
+++ scripts/components/InitialView.js
@@ -11,7 +11,13 @@ class InitialView extends React.Component {
     render() {
       return (
         <div className="initial-view-wrap">
-            <h1>You'll never go to a museum without it</h1>
+            <h1>Walk with history</h1>
+            <h2>Heyday</h2>
+
+            {/* benefit section component*/}
+
+            {/* about HeyDay section component*/}
+            
         </div>
       )
     }
