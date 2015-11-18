a3e46ae0d79abe11a442a4d312ba5801aa7c6838
Nathan Kane <kanenathan213@gmail.com>
2015-11-18 19:19:46 +0700

    1-more stock pics, benefit section, gradient overlay

--- scripts/components/InitialView.js
+++ scripts/components/InitialView.js
@@ -11,12 +11,34 @@ class InitialView extends React.Component {
     render() {
       return (
         <div className="initial-view-wrap">
-            <h1>HEYDAY</h1>
-            <h2>Walk with history</h2>
+            <header className="intro-header">
+                <div className="hero-img-overlay"></div>
+                <div className="logo-wrap">
+                    <div className="logo-name">HEYDAY</div>
+                    <div className="logo-tagline">Walk with history</div>
+                </div>
+            </header>
+            <section className="benefit benefit-1">
+                <div className="benefit-img-overlay"></div>
+                <div className="benefit-statement">
+                    Fascinating audio tours for historical places
+                </div>
+            </section>
+            <section className="benefit benefit-2">
+                <div className="benefit-img-overlay"></div>
+                <div className="benefit-statement">
+                    Easily listen from your phone
+                </div>
+            </section>
+
+            <section className="benefit benefit-3">
+                <div className="benefit-img-overlay"></div>
+                <div className="benefit-statement">
+                    Skip the line and save some money
+                </div>
+            </section>
 
             {/* benefit section component*/}
-
-            {/* about HeyDay section component*/}
 
         </div>
       )
