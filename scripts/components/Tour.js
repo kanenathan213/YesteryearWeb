a276c6edd157230fd854dd3178d5ed8a0587b067
Nathan Kane <kanenathan213@gmail.com>
2015-11-19 21:51:14 +0700

    2-basic list working, some styling

--- scripts/components/Tour.js
+++ scripts/components/Tour.js
@@ -4,8 +4,12 @@ class Tour extends React.Component {
     render() {
         return (
             <li>
-                { this.props.details.name }
-                <img src={ this.props.details.image } alt="tour-image" className="tour-thumbnail" />
+                <div className="tour-wrap">
+                    <img src={ this.props.details.image } alt="tour-image" className="tour-thumbnail" />
+                    <div className="tour-overlay"></div>
+                    <span className="tour-name">{ this.props.details.name }</span>
+                </div>
+
             </li>
         )
     }
