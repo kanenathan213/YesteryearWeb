4b9f840632dc76a5592a3cb2f1e3c4eb32509d28
Nathan Kane <kanenathan213@gmail.com>
2015-11-19 23:08:02 +0700

    2-making find tour page responsive

--- scripts/components/Tour.js
+++ scripts/components/Tour.js
@@ -8,8 +8,9 @@ class Tour extends React.Component {
                     <img src={ this.props.details.image } alt="tour-image" className="tour-thumbnail" />
                     <div className="tour-overlay"></div>
                     <span className="tour-name">{ this.props.details.name }</span>
+                    <span className="tour-price">${ this.props.details.price }</span>
+                    <span className="tour-stops">{ this.props.details.totalTime } min / { this.props.details.numberOfStops } stops </span>
                 </div>
-
             </li>
         )
     }
--- scripts/sample-tours.js
+++ scripts/sample-tours.js
@@ -1,32 +1,57 @@
 module.exports = {
     tour1 : {
         name : 'The Louvre',
-        number_of_stops : 17,
+        numberOfStops : 17,
         price : 8,
         lat : 48.8606111,
         long : 2.337644,
         description : 'Former historic palace housing a huge art collection, from Roman sculptures to Da Vinci\'s Mona Lisa.',
         image : 'https://i.imgur.com/Se8fKRA.jpg',
-        isPurchased : false
+        isPurchased : false,
+        totalTime : 245
     },
     tour2 : {
         name : 'Picasso Museum',
-        number_of_stops : 12,
+        numberOfStops : 12,
         price : 5,
         lat : 41.385216,
         long : 2.1808927,
         description : 'Medieval mansions housing extensive collection of influential Spanish cubist\'s works & masterpieces.',
         image : 'https://i.imgur.com/EF4MNNi.jpg',
-        isPurchased : true
+        isPurchased : true,
+        totalTime : 125
     },
     tour3 : {
         name : 'The Prado',
-        number_of_stops : 19,
+        numberOfStops : 19,
         price : 12,
         lat : 40.4137818,
         long : -3.6921271,
         description : 'Art museum with Velazquez, Goya and El Greco masterpieces in vast 12th to 19th-century collection.',
         image : 'https://i.imgur.com/YNyG3TYb.jpg',
-        isPurchased : false
+        isPurchased : false,
+        totalTime : 57
+    },
+    tour4 : {
+        name : 'SFMOMA',
+        numberOfStops : 17,
+        price : 8,
+        lat : 48.8606111,
+        long : 2.337644,
+        description : 'Former historic palace housing a huge art collection, from Roman sculptures to Da Vinci\'s Mona Lisa.',
+        image : 'https://i.imgur.com/Se8fKRA.jpg',
+        isPurchased : false,
+        totalTime : 113
+    },
+    tour5 : {
+        name : 'New York Met',
+        numberOfStops : 12,
+        price : 5,
+        lat : 41.385216,
+        long : 2.1808927,
+        description : 'Medieval mansions housing extensive collection of influential Spanish cubist\'s works & masterpieces.',
+        image : 'https://i.imgur.com/EF4MNNi.jpg',
+        isPurchased : true,
+        totalTime : 26
     }
 };
