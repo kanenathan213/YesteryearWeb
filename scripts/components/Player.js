0617edfad46275d4bb38e013dd8936cf8ba89a5b
Nathan Kane <kanenathan213@gmail.com>
2015-11-23 17:27:44 +0700

    4-put progress component in Player, added progress tracking method to update Player state when playing

--- scripts/components/Player.js
+++ scripts/components/Player.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import autobind from 'autobind-decorator';
 
+import ProgressLabel from '../../node_modules/react-progress-label/dist/progress-label';
+
 @autobind
 class Track extends Component {
 
@@ -10,7 +12,8 @@ class Track extends Component {
       this.state = {
           elem: null,
           isPlaying : false,
-          isLoading : false
+          isLoading : false,
+          progress : 0
       };
       this.playerEvent = {};
     }
@@ -58,8 +61,7 @@ class Track extends Component {
         this.setState({
           isPlaying: true
         });
-        console.log(this.audioTag.duration);
-        console.log(this.audioTag.currentTime);
+        this._progressTracking("start");
       }
 
       _stop() {
@@ -67,11 +69,46 @@ class Track extends Component {
         this.setState({
           isPlaying: false
         });
+        this._progressTracking("stop");
+      }
+
+      _progressTracking(action) {
+          let progressUpdate;
+          if (action === "start") {
+              progressUpdate = setInterval(this._updateProgress, 1000);
+          }
+          else if ((action === "stop") && progressUpdate !== undefined) {
+              clearInterval(progressUpdate);
+          }
+      }
+
+      _updateProgress() {
+          let duration = this.audioTag.duration;
+          let currentTime = this.audioTag.currentTime;
+          let newProgress = (currentTime / duration ) * 100;
+          this.setState({
+            progress: newProgress
+          });
+          console.log(this.state.progress);
       }
 
       render() {
+        let progress = 0;
         return (
             <div className="player">
+                <div className="progress-bar-wrapper">
+                    <ProgressLabel
+                        progress={this.state.progress}
+                        startDegree={0}
+                        progressWidth={5}
+                        trackWidth={4}
+                        cornersWidth={0}
+                        size={50}
+                        fillColor="transparent"
+                        trackColor="hsla(206, 100%, 15%, 1)"
+                        progressColor="hsla(82, 0%, 93%, 1)">
+                    </ProgressLabel>
+                </div>
                 { !this.state.isPlaying && !this.state.isLoading ?
                     <i className="ion-ios-play tour-control" onClick={this._play} /> : null
                 }
--- scripts/components/TourStop.js
+++ scripts/components/TourStop.js
@@ -3,7 +3,6 @@ import ReactDOM  from 'react-dom';
 import reactMixin from 'react-mixin';
 import autobind from 'autobind-decorator';
 
-import ProgressLabel from '../../node_modules/react-progress-label/dist/progress-label';
 import Player from './Player';
 
 @autobind
@@ -11,44 +10,31 @@ class TourStop extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            seeking: false,
+            playing: false,
+            currentTime: 24,
+            progressVal: 8,
+            duration: 300
+        };
     }
 
     render() {
-        let progress = 0;
-        let textStyle = {
-            'fill': '#000',
-            'textAnchor': 'middle'
-        };
         return (
             <li className="tour-stop-wrap" ref="tourID">
 
-                    <figure className="stop-figure">
-                        <img src={ this.props.stopDetails.image } alt="tour-image" className="stop-thumbnail" />
-                    </figure>
-                    <div className="stop-text-wrapper">
-                        <span className="stop-name">{ this.props.stopDetails.name }</span>
-                    </div>
-                    <div className="progress-bar-wrapper">
-                        <ProgressLabel
-                            progress={progress}
-                            startDegree={0}
-                            progressWidth={5}
-                            trackWidth={4}
-                            cornersWidth={0}
-                            size={50}
-                            fillColor="transparent"
-                            trackColor="hsla(206, 100%, 15%, 1)"
-                            progressColor="hsla(82, 0%, 93%, 1)">
+                <figure className="stop-figure">
+                    <img src={ this.props.stopDetails.image } alt="tour-image" className="stop-thumbnail" />
+                </figure>
+                <div className="stop-text-wrapper">
+                    <span className="stop-name">{ this.props.stopDetails.name }</span>
+                </div>
+                <Player
+                    stopAll={this.props.stopAll}
+                    ptag={this.props.ptag}
+                    stopDetails={this.props.stopDetails}
+                />
 
-                            <text x="200" y="200" style={textStyle}>
-                            </text>
-                        </ProgressLabel>
-                        <Player
-                            stopAll={this.props.stopAll}
-                            ptag={this.props.ptag}
-                            stopDetails={this.props.stopDetails}
-                        />
-                    </div>
             </li>
         )
     }
